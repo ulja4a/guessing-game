c88c9303264ed5202c5fc599b6b3c74a014e6a55
ulja4a <ulja4a@gmail.com>
2023-10-19 20:42:56 +0400

    100 passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessNumber = 0;
+      }
+    
     setRange(min, max) {
-
-    }
+        this.min = min;
+        this.max = max;
+      }
 
     guess() {
-
+        this.guessNumber = Math.round((this.max + this.min)/2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
